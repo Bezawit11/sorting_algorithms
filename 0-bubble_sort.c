53310f7487518eccbfdf07699825da949e281875
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-04 08:20:09 +0000

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,15 +11,15 @@ void bubble_sort(int *array, size_t size)
 {
 size_t i, j;
 int a;
-for (i = 0; i < size; i++)
-{
-for (j = 0; j < size; j++)
-{
-if (array[i] < array[j])
-{
-a = array[i];
-array[i] = array[j];
-array[j] = a;
+for(i = 0; i < size - 1; i++)
+{   
+for(j = 0; j < size - i - 1; j++)
+{          
+if(array[j] > array[j + 1])
+{               
+a = array[j];
+array[j] = array[j + 1];
+array[j + 1] = a;
 print_array(array, size);
 }
 }
