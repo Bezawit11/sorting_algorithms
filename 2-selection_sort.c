0a904e128907467a3deb1b4d58c8ed7e28b0f743
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-05 15:02:09 +0000

    Update 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -9,19 +9,19 @@ void selection_sort(int *array, size_t size)
 {
 size_t i, j, k = 0;
 int a;
-for (i = 0; i < size - 1; i++){
-    a = array[i];
-for (j = i + 1; j < size; j++){
-if (array[i] > array[j]){
-if (array[j] < a){
-    a = array[j];
-    k = j;
-    }
+for (i = 0; i < size - 1; i++)
+{
+a = array[i];
+for (j = i; j < size; j++)
+{
+if (a > array[j])
+{
+a = array[j];
+k = j;
 }
 }
 array[k] = array[i];
 array[i] = a;
-if (i != k){
-print_array(array, size);}
+print_array(array, size);
 }
 }
