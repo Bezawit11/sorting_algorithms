dfdb0d2e07f8d616f3a24c19e72e724d9f955f2d
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-07 17:39:03 +0000

    Update 100-shell_sort.c

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,12 +7,13 @@
  */
 void shell_sort(int *array, size_t size)
 {
-int temp = array[i];
+int temp;
 size_t j, i, interval;
 for (interval = size / 2; interval > 0; interval /= 2) 
 {
 for (i = interval; i < size; i += 1) 
 {
+temp = array[i];
 for (j = i; j >= interval && array[j - interval] > temp; j -= interval)
 {
 array[j] = array[j - interval];
