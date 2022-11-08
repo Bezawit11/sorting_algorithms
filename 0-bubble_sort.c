6fc0fe9e940be161defaf63aa88e07e26ffedcfd
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-08 18:48:12 +0000

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -15,12 +15,12 @@ if (array == NULL)
 {
 return;
 }
-for(i = 0; i < size - 1; i++)
-{   
-for(j = 0; j < size - i - 1; j++)
-{          
-if(array[j] > array[j + 1])
-{               
+for (i = 0; i < size - 1; i++)
+{
+for (j = 0; j < size - i - 1; j++)
+{        
+if (array[j] > array[j + 1])
+{
 a = array[j];
 array[j] = array[j + 1];
 array[j + 1] = a;
