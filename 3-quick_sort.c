232665a809ec03f7e8db8cf5627c64d8b3467ee2
Bezawit11 <socbeza13@gmail.com>
2022-11-08 05:50:03 -0800

    quick

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,46 +1,91 @@
 #include "sort.h"
 
+
+
 /**
  *
+ *  *
  *
+ *   *
  *
- */
+ *    *
+ *
+ *     */
+
  void quick_sort(int *array, size_t size)
 
+
+
 {
-int a = size;
-if (array == NULL)
-{
-return;
+
+	int a = size;
+
+	if (array == NULL)
+
+	{
+
+		return;
+
+	}
+
+	quick_sort_helper(array, 0, a - 1, a);
+
 }
-quick_sort_helper(array, 0, a - 1, a);
-}
+
+
+
 
 
 /**
  *
+ *  *
  *
+ *   *
  *
- */
+ *    *
+ *
+ *     */
+
 void quick_sort_helper(int *array, int low , int high, int size)
 
+
+
 {
-int p = array[high];
-int j = low, i, a;
-if (low >= high)
-{
-return;
-}
-for (i = low; i <= high; i++){
-if (array[i] <= p){
-a = array[i];
-array[i] = array[j];
-array[j] = a;
-j++;
-if (j - 1 != i){
-print_array(array, size);}
-}
-}
-quick_sort_helper(array, low, j - 2, size);
-quick_sort_helper(array, j, high, size);
+
+	int p = array[high];
+
+	int j = low, i, a;
+
+	if (low >= high)
+
+	{
+
+		return;
+
+	}
+
+	for (i = low; i <= high; i++){
+
+		if (array[i] <= p){
+
+			a = array[i];
+
+			array[i] = array[j];
+
+			array[j] = a;
+
+			j++;
+
+			if (j - 1 != i){
+
+				print_array(array, size);}
+
+		}
+
+	}
+
+	quick_sort_helper(array, low, j - 2, size);
+
+	quick_sort_helper(array, j, high, size);
+
 }
