ac4cd370dba24dfb9fead888f0eea5800327cf44
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-08 19:09:33 +0000

    Update 3-quick_sort.c

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,9 +1,10 @@
 #include "sort.h"
 
 /**
- *
- *  
- *
+ *quick_sort - a function that sorts using the Quick sort algorithm
+ *@array: array of integers to be sorted
+ *@size: size of the array
+ *Return: void
  */
 void quick_sort(int *array, size_t size)
 
@@ -17,9 +18,11 @@ quick_sort_helper(array, 0, a - 1, a);
 }
 
 /**
- *
- * 
- *
+ *quick_sort_helper - a recursive function that partitions an array and quick sorts
+ *@array: array of integers to be sorted
+ *@low: the lowest position
+ *@high: the highest position
+ *Return: void
  */
 void quick_sort_helper(int *array, int low , int high, int size)
 
@@ -37,7 +40,7 @@ if (array[i] <= p)
 a = array[i];
 array[i] = array[j];
 array[j] = a;
-j++;
+j++; 
 if (j - 1 != i)
 {
 print_array(array, size);}
