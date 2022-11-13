ef1df86951ee9f4d98ac1c57f773d4de0d27bef1
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-13 14:16:55 +0000

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -11,22 +11,23 @@ void insertion_sort_list(listint_t **list)
 listint_t *tmpp = malloc(sizeof(listint_t));
 listint_t *tmpn = malloc(sizeof(listint_t));
 listint_t *try = malloc(sizeof(listint_t));
-listint_t *new = malloc(sizeof(listint_t));
 try = *list;
 while (try->next != NULL){
 if (try->n > try->next->n){
-//printf("%d > %d\n", try->n, try->next->n);
-tmpp = try->prev; //null
-tmpn = try->next; //13
+tmpp = try->prev;
+tmpn = try->next;
 if (tmpp != NULL){
 tmpp->next = tmpn;}
 tmpn->prev = tmpp;
 try->next = tmpn->next;
 tmpn->next = try;
 try->prev = tmpn;
-if (try->next != NULL){
-try->next->prev = try;}
-if (tmpp == NULL){
+if (try->next != NULL)
+{
+try->next->prev = try;
+}
+if (tmpp == NULL)
+{
 (*list) = tmpn;
 }
 print_list(*list);
