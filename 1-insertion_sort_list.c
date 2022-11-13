fafeadec9937666b5da6d5a38ea442b9fd8c090c
Bezawit11 <106753525+Bezawit11@users.noreply.github.com>
2022-11-13 12:48:32 +0000

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,12 +1,38 @@
 #include "sort.h"
 
 /**
- *
- *
- *
- *
+ *insertion_sort_list - function that sorts a doubly linked list
+ *@list: doubly linked list to be sorted
+ *Return: void
  */
 void insertion_sort_list(listint_t **list)
 
 {
+listint_t *tmpp = malloc(sizeof(listint_t));
+listint_t *tmpn = malloc(sizeof(listint_t));
+listint_t *try = malloc(sizeof(listint_t));
+listint_t *new = malloc(sizeof(listint_t));
+try = *list;
+while (try->next != NULL){
+if (try->n > try->next->n){
+//printf("%d > %d\n", try->n, try->next->n);
+tmpp = try->prev; //null
+tmpn = try->next; //13
+if (tmpp != NULL){
+tmpp->next = tmpn;}
+tmpn->prev = tmpp;
+try->next = tmpn->next;
+tmpn->next = try;
+try->prev = tmpn;
+if (try->next != NULL){
+try->next->prev = try;}
+if (tmpp == NULL){
+(*list) = tmpn;
+}
+print_list(*list);
+try = *list;
+continue;
+}
+try = try->next;
+}
 }
